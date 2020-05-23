a7e8aedbd818b35bfaf91cf98d9ceb63a21ff0db
gaponukz <gaponukz54@hmail.com>
2020-05-23 18:02:06 +0300

    Updates

---
+++ precache-manifest.694579600af064d4b6ccd79973582856.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4745a6685b26aa51372f5288006d23fe",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "1d75897eefffe849543d",
+    "url": "/to-do/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7fec6422a5f8fa5412d6",
+    "url": "/to-do/static/js/2.ea72b22f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to-do/static/js/2.ea72b22f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d75897eefffe849543d",
+    "url": "/to-do/static/js/main.8b2ea65f.chunk.js"
+  },
+  {
+    "revision": "3c5dbf546597a235a261",
+    "url": "/to-do/static/js/runtime-main.27ca5ea1.js"
+  }
+]);
